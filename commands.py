02d3b1b8ff34ea3c3ef4a19327a3824bbd575094
Artem <46228212+lapakota@users.noreply.github.com>
2020-10-23 23:04:20 +0500

    Add files via upload

--- commands.py
+++ commands.py
@@ -2,93 +2,77 @@ from arg_parser import ArgParser
 from storage import Storage
 
 
-def execute_command(storage, input_data):
-    print(input_data)
-    str_command, arg1, arg2 = ArgParser.parse_args(input_data)
-    try:
-        command = COMMANDS[str_command]
-    except KeyError:
-        return 'Error with command'
-    else:
-        return command(storage, arg1, arg2)
+class Commands:
+    def __init__(self):
+        self.COMMANDS = {
+            'add': self.add_pair,
+            'del': self.delete_pair,
+            'get': self.get_value,
+            'save': self.save_storage,
+            'change': self.change_storage,
+            'all': self.get_all_data,
+            'exit': self.close_app,
+            'clone': self.clone_storage,
+        }
 
+    def handle_command(self, command, *args):
+        return self.COMMANDS[command](*args)
 
-def add_pair(storage, key, value):
-    try:
-        storage.add(key, value)
-        return f"Pair {key}:{value} was added"
-    except Exception as e:
-        return e
+    def execute_command(self, storage, input_data):
+        str_command, arg1, arg2 = ArgParser.parse_args(input_data)
+        try:
+            return self.handle_command(str_command, storage, arg1, arg2)
+        except KeyError:
+            return 'Error with input data'
 
+    def add_pair(self, storage, key, value):
+        try:
+            storage.add(key, value)
+            return f"Pair {key}:{value} was added"
+        except Exception as e:
+            return e
 
-def delete_pair(storage, key, arg=None):
-    try:
-        value = storage.data[key]
-        storage.delete(key)
-        return f"{key}:{value} was deleted"
-    except KeyError:
-        return 'Key not found'
+    def delete_pair(self, storage, key, arg=None):
+        try:
+            value = storage.data[key]
+            storage.delete(key)
+            return f"{key}:{value} was deleted"
+        except KeyError:
+            return 'Key not found'
 
+    def get_value(self, storage, key, arg=None):
+        try:
+            return storage.get(key)
+        except KeyError:
+            return 'Key not found'
 
-def get_value(storage, key, arg=None):
-    try:
-        return storage.get(key)
-    except KeyError:
-        return 'Key not found'
+    def save_storage(self, storage, arg1=None, arg2=None):
+        try:
+            storage.save()
+            return "State was saved"
+        except Exception as e:
+            return e
 
+    def change_storage(self, storage, name, arg=None):
+        try:
+            storage.change_storage(name)
+            return f'Storage {name} selected'
+        except Exception as e:
+            return e
 
-def save_storage(storage, arg1=None, arg2=None):
-    try:
-        storage.save()
-        return "State was saved"
-    except Exception as e:
-        return e
+    def get_all_data(self, storage, arg1=None, arg2=None):
+        return storage.data
 
+    def close_app(self, storage=None, arg1=None, arg2=None):
+        print('Good bye')
+        exit(0)
 
-def load_storage(storage):
-    try:
-        storage.load()
-        return 'Storage was loaded'
-    except Exception as e:
-        return e
-
-
-def change_storage(storage, name, arg=None):
-    try:
-        storage.change_storage(name)
-        return f'Storage {name} selected'
-    except Exception as e:
-        return e
-
-
-def get_all_data(storage, arg1=None, arg2=None):
-    return storage.data
-
-
-def close_app(storage=None, arg1=None, arg2=None):
-    print('Good bye')
-    exit(0)
-
-
-def clone_storage(storage, arg1=None, arg2=None):
-    try:
-        new_name = storage.name + '-clone'
-        new_storage = Storage(new_name)
-        new_storage.data = storage.data
-        new_storage.save()
-        return f"Clone {new_name} was created"
-    except Exception as e:
-        return e
-
-
-COMMANDS = {
-    'add': add_pair,
-    'del': delete_pair,
-    'get': get_value,
-    'save': save_storage,
-    'load': load_storage,
-    'change': change_storage,
-    'all': get_all_data,
-    'exit': close_app,
-    'clone': clone_storage,
-}
+    def clone_storage(self, storage, arg1=None, arg2=None):
+        try:
+            new_name = storage.name + '-clone'
+            new_storage = Storage(new_name)
+            new_storage.data = storage.data
+            new_storage.save()
+            return f"Clone {new_name} was created"
+        except Exception as e:
+            return e
